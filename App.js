b57f8c0910c50704231aac4c9896f7545e9952fa
Hiago Souza <hiagofss98@gmail.com>
2019-07-05 20:18:40 -0300

    ESLint, Prettier & EditorConfig

--- App.js
+++ App.js
@@ -1,16 +1,16 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
-    textAlign: "center",
+    textAlign: 'center',
     margin: 10,
   },
 });
